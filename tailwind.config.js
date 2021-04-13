22b97078adef37e3650b6417293ec0c7df59b9aa
bhimrazy <bhimrajyadav977@gmail.com>
2021-04-13 14:00:10 +0545

    a sample dashboard made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
+const colors = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+        poppins: ["Poppins", "system-ui", "sans-serif"],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray,
+        cool: colors.coolGray,
+        bluegray: colors.blueGray,
+        red: colors.red,
+        blue: colors.lightBlue,
+        yellow: colors.amber,
+        teal: colors.teal,
+        green: colors.green,
+        emerald: colors.emerald,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
